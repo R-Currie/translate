4090688a9827050b98c40284cd37112a4cdd4d56
R-Currie <currierobertm@gmail.com>
2019-11-08 14:52:23 -0600

    initial commit, basic template with no functionality

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import UserCreate from './UserCreate';
+
+class App extends React.Component {
+    state = { language: 'english' }
+
+    onLanguageChange = (language) => {
+        this.setState({ language: language });
+    }
+
+    render(){
+        return(
+            <div className="ui container">
+                <div>
+                    Select a language:
+                    <i className="flag uk" onClick={() => this.onLanguageChange('english')} />
+                    <i className="flag es" onClick={() => this.onLanguageChange('spanish')} />
+                </div>
+                <UserCreate />
+            </div>
+        )
+    }
+}
+
+export default App
