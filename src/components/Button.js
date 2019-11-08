c7cff13e3cea1597728aa93e15ef67e62a8cadd3
R-Currie <currierobertm@gmail.com>
2019-11-08 15:37:01 -0600

    The context system is set up, field and button use different syntax, but are functionally the same

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component{
+
     render(){
         return(
             <div>
-                <button className="ui button primary">Submit</button>
+                <button className="ui button primary">
+                    <LanguageContext.Consumer>
+                        {(value) => value === 'english' ? 'Submit' : 'Enviar'}
+                    </LanguageContext.Consumer>
+                </button>
             </div>
         )
     }
